40a096375d74b6fb598504ca152dfce9bd919020
Mahmud Toheeb Adebayo <adebayot971@gmail.com>
2026-08-13 21:04:44 +0100

    modify string js

--- string.js
+++ string.js
@@ -30,6 +30,24 @@ console.log(filename.endsWith(".pdf"));     // true
 
 console.log(filename.endsWith(".docx"));    // false
 
+const extractExample = "Esther"
+
+console.log(extractExample.slice(0, 3)); // "Est"
+
+console.log(extractExample.slice(3));    // "her"
+
+console.log(extractExample.slice(-3));  // "her"
+
+console.log(extractExample.substring(0, 3)); // "Est"
+
+console.log(extractExample.substring(3));    // "her"
+
+console.log(extractExample.substring(-3));  // "Esther" (negative index treated as 0)
+
+
+
+
+
 
 
 
